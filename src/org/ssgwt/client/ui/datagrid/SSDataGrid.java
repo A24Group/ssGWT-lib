fd3645335e73489a4374aea61726198847372e05
Michael <michael@michael-laptop.(none)>
2012-07-02 12:02:35 +0200

    Added some functionallity for the SSDataGrid
    Added required methods for the SSDataGrid. A few things
    are still required so this is a Work in progress.
    
    issue #12

--- src/org/ssgwt/client/ui/datagrid/SSDataGrid.java
+++ src/org/ssgwt/client/ui/datagrid/SSDataGrid.java
@@ -2,13 +2,20 @@ package org.ssgwt.client.ui.datagrid;
 
 import java.util.List;
 
+import org.eclipse.jdt.internal.compiler.ast.DoStatement;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.layout.client.Layout.Layer;
+import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.DataGrid;
+import com.google.gwt.user.cellview.client.Header;
 import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.cellview.client.SimplePager.TextLocation;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RequiresResize;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -32,6 +39,11 @@ public class SSDataGrid<T> extends Composite implements RequiresResize {
     }
 
     /**
+     * Whether the data grid supports selecting multiple rows
+     */
+    private boolean multiSelect;
+    
+    /**
      * Instance of the UiBinder
      */
     private static Binder uiBinder = GWT.create(Binder.class);
@@ -40,18 +52,22 @@ public class SSDataGrid<T> extends Composite implements RequiresResize {
      * The DataGrid that will be displayed on the screen
      */
     @UiField(provided = true)
-    protected DataGrid<T> dataGrid = new DataGrid<T>();
-
+    protected DataGrid<T> dataGrid;
+    
     /**
-     * The resource for the SimplePager
+     * The DataGrid that will be displayed on the screen
      */
-    protected SimplePager.Resources pagerResources = GWT.create(SimplePager.Resources.class);
+    @UiField
+    protected FlowPanel actionBar;
 
+    @UiField
+    FlowPanel actionBarContainer;
+    
     /**
      * The pager that will handle the paging of the DataGrid
      */
     @UiField(provided = true)
-    protected SimplePager pager = new SimplePager(TextLocation.CENTER, pagerResources, false, 0, true);
+    protected SimplePager pager;
 
     /**
      * Class Constructor
@@ -60,13 +76,20 @@ public class SSDataGrid<T> extends Composite implements RequiresResize {
      * @since 29 June 2012
      */
     public SSDataGrid() {
+        this((DataGrid.Resources)GWT.create(DataGrid.Resources.class), (SimplePager.Resources)GWT.create(SimplePager.Resources.class));
+    }
+    
+    /**
+     * Class Constructor
+     * 
+     * @author Lodewyk Duminy
+     * @since 29 June 2012
+     */
+    public SSDataGrid(DataGrid.Resources dataGridResource, SimplePager.Resources pagerResource) {
         this.initWidget(uiBinder.createAndBindUi(this));
-        
+        dataGrid = new DataGrid<T>(10, dataGridResource);
+        pager = new SimplePager(TextLocation.CENTER, pagerResource, false, 0, true);
         pager.setDisplay(dataGrid);
-        pager.setPageSize(10);
-        
-        this.setData(null);
-        
     }
 
     /**
@@ -102,6 +125,153 @@ public class SSDataGrid<T> extends Composite implements RequiresResize {
      */
     @Override
     public void onResize() {
-        dataGrid.setHeight((this.getOffsetHeight( ) - 40) + "px");
+        dataGrid.setHeight((this.getOffsetHeight() - 40) + "px");
     }
+    
+    /**
+     * Adds a column to the end of the table.
+     * 
+     * @param col the column to be added
+     */
+    public void addColumn(Column<T, ?> col) {
+        dataGrid.addColumn(col);
+    }
+
+    /**
+     * Adds a column to the end of the table with an associated header.
+     * 
+     * @param col the column to be added
+     * @param header the associated {@link Header}
+     */
+    public void addColumn(Column<T, ?> col, Header<?> header) {
+        dataGrid.addColumn(col, header);
+    }
+
+    /**
+     * Adds a column to the end of the table with an associated header and footer.
+     * 
+     * @param col the column to be added
+     * @param header the associated {@link Header}
+     * @param footer the associated footer (as a {@link Header} object)
+     */
+    public void addColumn(Column<T, ?> col, Header<?> header, Header<?> footer) {
+        dataGrid.addColumn(col, header, footer);
+    }
+
+    /**
+     * Adds a column to the end of the table with an associated String header.
+     * 
+     * @param col the column to be added
+     * @param headerString the associated header text, as a String
+     */
+    public void addColumn(Column<T, ?> col, String headerString) {
+        dataGrid.addColumn(col, headerString);
+    }
+
+    /**
+     * Adds a column to the end of the table with an associated {@link SafeHtml}
+     * header.
+     * 
+     * @param col the column to be added
+     * @param headerHtml the associated header text, as safe HTML
+     */
+    public void addColumn(Column<T, ?> col, SafeHtml headerHtml) {
+        dataGrid.addColumn(col, headerHtml);
+    }
+
+    /**
+     * Adds a column to the end of the table with an associated String header and
+     * footer.
+     * 
+     * @param col the column to be added
+     * @param headerString the associated header text, as a String
+     * @param footerString the associated footer text, as a String
+     */
+    public void addColumn(Column<T, ?> col, String headerString, String footerString) {
+        dataGrid.addColumn(col, headerString, footerString);
+    }
+
+    /**
+     * Adds a column to the end of the table with an associated {@link SafeHtml}
+     * header and footer.
+     * 
+     * @param col the column to be added
+     * @param headerHtml the associated header text, as safe HTML
+     * @param footerHtml the associated footer text, as safe HTML
+     */
+    public void addColumn(Column<T, ?> col, SafeHtml headerHtml, SafeHtml footerHtml) {
+        dataGrid.addColumn(col, headerHtml, footerHtml);
+    }
+    
+    /**
+     * Sets the amount of rows that the data grid will have. This
+     * does not specify how many rows will be displayed.
+     * 
+     * @param count - The amount of rows that the data grid will have
+     */
+    public final void setRowCount(int count) {
+        dataGrid.setRowCount(count);
+    }
+
+    /**
+     * Sets the amount of rows that the data grid will have. This
+     * does not specify how many rows will be displayed.
+     * 
+     * @param count - The amount of rows that the data grid will have
+     * @param isExact - Whether or not the row measurement is exact
+     */
+    public void setRowCount(int size, boolean isExact) {
+        dataGrid.setRowCount(size, isExact);
+    }
+    
+    /**
+     * Hides the action bar by setting it invisible
+     */
+    public void hideActionBar(){
+        actionBar.setVisible(false);
+    }
+    
+    /**
+     * Hides the header by setting it invisible
+     */
+    public void hideHeader(){
+        //TODO: Add functionality for reusability
+    }
+    
+    /**
+     * Adds a widget to the action bar
+     * 
+     * @param actionBarWidget - The widget that needs to be added to the action bar
+     */
+    public void setActionBarWidget(Widget actionBarWidget){
+        this.actionBarContainer.add(actionBarWidget);
+    }
+    
+    /**
+     * Sets the size of the pager.
+     * 
+     * @param pageSize - The size of the pager
+     */
+    public void setPageSize(int pageSize) {
+      pager.setPageSize(pageSize);
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public boolean isMultiSelect(){
+        return multiSelect;
+    }
+    
+    /**
+     * Set whether or not the grid should support multiple row select
+     * 
+     * @param multiSelect - Whether the data grid should support multiple row select
+     */
+    public void setMultiSelect(boolean multiSelect){
+        this.multiSelect = multiSelect;
+        //TODO: Waiting for some stuff from Ruan
+    }
+    
 }
