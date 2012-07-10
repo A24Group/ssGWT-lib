c6e2dfe0e338956c055401360ddd1771c81c03a8
michaelbarnard <michael.barnard@a24group.com>
2012-07-10 12:55:34 +0200

    Added the licenses and a setMenuItem method
    issue #36

--- src/org/ssgwt/client/ui/menu/TopMenuBar.java
+++ src/org/ssgwt/client/ui/menu/TopMenuBar.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright 2012 A24Group
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
 package org.ssgwt.client.ui.menu;
 
 import java.util.ArrayList;
@@ -132,6 +145,16 @@ public class TopMenuBar extends Composite {
         this.resources = resources;
         this.resources.topMenuStyle().ensureInjected();
         this.initWidget(uiBinder.createAndBindUi(this));
+        this.setTopMenuBar(menuItems);
+    }
+    
+    /**
+     * Setter for the menu Items
+     * 
+     * @param menuItems - The list of menu items that needs to be added to the menu bar
+     */
+    public void setTopMenuBar(List<MenuItem> menuItems) {
+        topMenu.clear();
         if (menuItems != null) {
             List<MenuItem> sorted = new ArrayList<MenuItem>();
             int max = 0;
