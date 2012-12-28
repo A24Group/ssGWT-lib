f05ddfdde75bf36dffe890e1a26552144f97d802
Ryno Hartzer <ryno.hartzer@a24group.com>
2012-12-28 15:34:12 +0200

    Applied the cursor effect on the container
    Applied the cursor effect on the container.
    Removed the style used
    
    issue A24Group/Triage#2197

--- src/org/ssgwt/client/ui/LoaderImageButton.java
+++ src/org/ssgwt/client/ui/LoaderImageButton.java
@@ -107,12 +107,6 @@ public class LoaderImageButton extends FocusPanel implements ClickHandler {
     private String disabledStyle;
     
     /**
-     * Style used to turn the cursor into a hand when the cursor is over
-     * the component
-     */
-    private String handPointerStyle = "handPointer";
-    
-    /**
      * Class constructor creates a button with the image on the left side
      * 
      * @param label - The label of the button
@@ -249,7 +243,7 @@ public class LoaderImageButton extends FocusPanel implements ClickHandler {
         this.getElement().getStyle().setProperty("display", "inline-block");
         this.getElement().getStyle().setProperty("textAlign", "center");
         this.add(innerContainer);
-        innerContainer.addStyleName(handPointerStyle);
+        innerContainer.getElement().getStyle().setProperty("cursor", "pointer");
         innerContainer.getElement().getStyle().setProperty("display", "table");
         innerContainer.setSize("100%", "100%");
         activeStateContainer.getElement().getStyle().setProperty("display", "table-cell");
