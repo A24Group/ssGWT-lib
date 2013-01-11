94d08889c015c676b4e092c3e12c02726038c103
michaelbarnard <michael.barnard@a24group.com>
2013-01-11 08:25:30 +0200

    Added a skeleton class for the select box filter
    issue: #1148

---
+++ src/org/ssgwt/client/ui/datagrid/filter/SelectBoxFilter.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2012 A24Group
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package org.ssgwt.client.ui.datagrid.filter;
+
+/**
+ * The text filter that can be used on the SSDataGrid with a FilterSortHeader
+ * 
+ * @author Michael Barnard <michael.barnard>
+ * @since  11 January 2012
+ */
+public class SelectBoxFilter extends AbstractHeaderFilter {
+
+    /**
+     * @TODO
+     */
+    public SelectBoxFilter() {
+    
+    }
+    
+    /**
+     * @TODO
+     */
+    @Override
+    public void setTitle(String title) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+     * @TODO
+     */
+    @Override
+    public Criteria getCriteria() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * @TODO
+     */
+    @Override
+    protected boolean checkFilterActive() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * @TODO
+     */
+    @Override
+    protected void updateCriteriaObject() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+     * @TODO
+     */
+    @Override
+    protected void setCriteriaObjectEmpty() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+     * @TODO
+     */
+    @Override
+    protected void updateFieldData() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+     * @TODO
+     */
+    @Override
+    protected void clearFields() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
