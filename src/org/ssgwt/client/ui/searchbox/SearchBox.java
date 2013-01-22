bcb9749eb767c537ac087b7446a326a96ea17b31
Johannes <johannes.gryffenberg@a24group.com>
2013-01-22 16:37:14 +0200

    Added handler for when the user types and the retrieveResult function
    Added handler for when the user types and the retrieveResult function
    
    issue A24Group/Triage#2401

--- src/org/ssgwt/client/ui/searchbox/SearchBox.java
+++ src/org/ssgwt/client/ui/searchbox/SearchBox.java
@@ -1,4 +1,19 @@
+/**
+ * Copyright 2012 A24Group
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
 package org.ssgwt.client.ui.searchbox;
+
+import java.util.List;
 
 import org.ssgwt.client.ui.ImageButton;
 import org.ssgwt.client.ui.searchbox.recorddisplays.SearchBoxRecordWidget;
@@ -6,11 +21,14 @@ import org.ssgwt.client.ui.searchbox.recorddisplays.SearchBoxRecordWidget;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.ClientBundle.Source;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.TextBox;
@@ -24,7 +42,7 @@ import com.google.gwt.user.client.ui.Widget;
  *
  * @param <T> The type of the VO that will hold the data displayed by the search widget
  */
-public abstract class SearchBox<T> extends Composite {
+public abstract class SearchBox<T> extends Composite implements KeyUpHandler, ClickHandler {
     
     /**
      * The minimum characters required for search service call to be made
@@ -69,6 +87,26 @@ public abstract class SearchBox<T> extends Composite {
     private SearchBoxRecordWidget<T> selectedDisplayItem;
     
     /**
+     * The request id
+     */
+    private int requestId;
+    
+    /**
+     * The string the last delay time was created for
+     */
+    private String previousSearchString = null;
+    
+    /**
+     * A delay time that allows us to prevent service calls if the user types to fast
+     */
+    private Timer delayedRequest = null;
+    
+    /**
+     * The number of milliseconds a call will be delayed after a key up event in order to prevent extra service calls
+     */
+    private int requestDelayTime = 100;
+    
+    /**
      * UiBinder interface for the composite
      * 
      * @author Johannes Gryffenberg <johannes.gryffenberg@gmail.com>
@@ -102,13 +140,8 @@ public abstract class SearchBox<T> extends Composite {
         this.resources.searchBoxStyle().ensureInjected();
         submitButton = new ImageButton(submitButtonLabel);
         this.initWidget(uiBinder.createAndBindUi(this));
-        submitButton.addClickHandler(new ClickHandler() {
-            
-            @Override
-            public void onClick(ClickEvent arg0) {
-                onSubmit(selectedObject);
-            }
-        });
+        submitButton.addClickHandler(this);
+        textBox.addKeyUpHandler(this);
     } 
     
     /**
@@ -209,6 +242,7 @@ public abstract class SearchBox<T> extends Composite {
      */
     public void setSelectedDisplayItem(SearchBoxRecordWidget<T> selectedDisplayItem) {
         this.selectedDisplayItem = selectedDisplayItem;
+        this.selectedObject = this.selectedDisplayItem.getItemVO();
     }
     
     /**
@@ -218,5 +252,66 @@ public abstract class SearchBox<T> extends Composite {
      */
     public void setMinCharRequiredForSearch(int minCharCount) {
         this.minCharCount = minCharCount;
+    }
+    
+    /**
+     * The key up event handler
+     * 
+     * @param event - The key up event that is being handled
+     */
+    @Override
+    public void onKeyUp(KeyUpEvent event){
+        if (textBox.getText().length() >= 3 && !textBox.getText().equals(previousSearchString)) {
+            previousSearchString = textBox.getText();
+            System.out.println("Search string " + textBox.getText());
+            if (delayedRequest != null) {
+                delayedRequest.cancel();
+                delayedRequest = null;
+            }
+            delayedRequest = new Timer() {
+                
+                @Override
+                public void run() {
+                    requestId++;
+                    System.out.println("Making request: " + textBox.getText() + " :: " +requestId);
+                    retrieveResult(textBox.getText(), requestId);
+                    delayedRequest = null;
+                }
+            };
+            delayedRequest.schedule(requestDelayTime);
+        } else if (textBox.getText().length() < 3) {
+            //TODO: Close the drop down popup and clear data
+        }
+    }
+    
+    /**
+     * The function that is called to start a service call to return search data
+     * 
+     * @param searchString - The search string the user entered
+     * @param requestId - The id of the request
+     */
+    public abstract void retrieveResult(String searchString, final int requestId);
+    
+    /**
+     * Set the data on the search box that was returned by the service call
+     * 
+     * @param searchResults - The search results
+     * @param requestId - The id of the request
+     */
+    public void setData(List<T> searchResults, int requestId) {
+        System.out.println("Set data check: " + requestId + " == " + this.requestId);
+        if (requestId == this.requestId) {
+            System.out.println("Set data for request: " + requestId);
+        }
+    }
+    
+    /**
+     * Handles the click events dispatched by the submit button
+     * 
+     * @param event - The click event that is being handled
+     */
+    @Override
+    public void onClick(ClickEvent event) {
+        onSubmit(selectedObject);
     }
 }
