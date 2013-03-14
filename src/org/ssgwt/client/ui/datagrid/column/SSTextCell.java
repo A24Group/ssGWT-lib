2f83cc87ffe118f480088634b568814bbf7d5742
ruanna <ruanna@dev-ruanna.(none)>
2013-03-14 13:51:23 +0200

    Updated the datagrid to not show "null"
    Updated the datagrid to not show "null"
    
    issue A24Group/Triage#2662

--- src/org/ssgwt/client/ui/datagrid/column/SSTextCell.java
+++ src/org/ssgwt/client/ui/datagrid/column/SSTextCell.java
@@ -17,9 +17,16 @@ public class SSTextCell extends AbstractCell<String> {
      * 
      * @author Ruan Naude <nauderuan777@gmail.com>
      * @since 14 March 2013
+     * 
+     * @param context -The context the cell is in
+     * @param value - The value to be added in the cell
+     * @param sb -The save html builder.
      */
     @Override
     public void render(Context context, String value, SafeHtmlBuilder sb) {
+        if (value == null) {
+            value = "";
+        }
         sb.appendHtmlConstant("<div title=\"" + value + "\" >" + value + "</div>");
     }
 
