127c8a07e6d5b91546a57dc1ff7e6cb170e621cf
Dmitri De Klerk <dmitriwarren@gmail.com>
2014-09-12 10:33:44 +0200

    Fix additional sort when clicking filter icon
    Fix additional sort happening when clicking the
    filter icon on firefox.
    
    #2674

--- src/org/ssgwt/client/ui/datagrid/FilterSortHeader.java
+++ src/org/ssgwt/client/ui/datagrid/FilterSortHeader.java
@@ -16,8 +16,17 @@ package org.ssgwt.client.ui.datagrid;
 import org.ssgwt.client.ui.datagrid.event.FilterChangeEvent;
 import org.ssgwt.client.ui.datagrid.filter.AbstractHeaderFilter;
 
+import com.google.gwt.cell.client.Cell;
+import com.google.gwt.cell.client.ValueUpdater;
+import com.google.gwt.cell.client.Cell.Context;
+import com.google.gwt.dom.client.BrowserEvents;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.ColumnSortEvent;
 import com.google.gwt.user.cellview.client.Header;
+import com.google.gwt.user.client.Window;
 
 /**
  * The header that is used to enable filtering on a table that uses the
@@ -64,4 +73,48 @@ public class FilterSortHeader extends Header<HeaderDetails> {
         return ((FilterSortCell)this.getCell()).addFilterChangeHandler(handler);
     }
     
+    /**
+     * Preview a browser event that may trigger a column sort event. Return true if the
+     * {@link CellTable} should proceed with sorting the column. Subclasses can override this method
+     * to disable column sort for some click events, or particular header/footer sections.
+     * <p>
+     * This method will be invoked even if the header's cell does not consume a click event.
+     * </p>
+     * 
+     * @param context the context of the header
+     * @param elem the parent Element
+     * @param event the native browser event
+     * @return true if the {@link CellTable} should continue respond to the event (i.e., if this is
+     *         a click event on a sortable column's header, fire {@link ColumnSortEvent}). False if
+     *         the {@link CellTable} should stop respond to the event. 
+     */
+    @Override
+    public boolean onPreviewColumnSortEvent(Context context, Element parent, NativeEvent event) {
+        Element filterImageElement = getImageElement(parent);
+        
+        // If the filter image element is clicked, then we don't want to perform a column sort event.
+        if (event.getEventTarget().equals(filterImageElement)) {
+            if (BrowserEvents.CLICK.equals(event.getType())) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * Retrieves the element of the image that is being displayed in the Cell
+     * 
+     * @author Dmitri De Klerk <dmitri.deklerk@a24group.com>
+     * @since  12 Sept 2014
+     * 
+     * @param parent - The top level container of the Cell
+     * 
+     * @return The element object that represents the image in the Cell
+     */
+    protected Element getImageElement(Element parent) {
+        if (parent.getFirstChildElement().getElementsByTagName("img").getItem(0).getAttribute("name").equals("filterIcon")) {
+            return parent.getFirstChildElement().getElementsByTagName("img").getItem(0);
+        }
+        return parent.getFirstChildElement().getElementsByTagName("img").getItem(1);
+    }
 }
